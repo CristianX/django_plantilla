31c31367e8b18c9a7afc38ef508188c320127cf8
CristianX <thecristianx@hotmail.com>
2023-05-30 21:21:00 -0500

    Personalizando formularios

--- motorInferencia/forms.py
+++ motorInferencia/forms.py
@@ -1,21 +1,33 @@
 from django import forms
 
-class FormArticle(forms.Form):
 
+class FormArticle(forms.Form):
     title = forms.CharField(
-        label = "Título",
+        label="Título",
+        max_length=40,
+        required=True,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Escribe el título", "class": "titulo_form_article"}
+        ),
     )
 
     content = forms.CharField(
-        label = "Contenido",
-        widget= forms.Textarea
+        label="Contenido",
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Ingrese el contendio",
+                "class": "contenido_form_article",
+                "id": "contendio_form",
+            }
+        ),
     )
+    # content.widget.attrs.update(
+    #     {
+    #         "placeholder": "Ingrese el contendio",
+    #         "class": "contenido_form_article",
+    #         "id": "contendio_form",
+    #     }
+    # )
 
-    public_options = [
-        (1, 'Si'),
-        (2, 'No')
-    ]
-    public = forms.TypedChoiceField(
-        label = "¿Publicado?",
-        choices = public_options
-    )
+    public_options = [(1, "Si"), (2, "No")]
+    public = forms.TypedChoiceField(label="¿Publicado?", choices=public_options)
